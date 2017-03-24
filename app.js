1843afbb6c5aee8c2098aef93a273dfbf6223526
Ashish Goel <asgoel@microsoft.com>
2017-03-24 12:26:14 -0700

    updated bot

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: 279f8bfb-a341-4e2b-9ebc-8247f446b244,
-    appPassword: h19bWpWSO8XsJZu89Cf8ntp
+    appId: "279f8bfb-a341-4e2b-9ebc-8247f446b244",
+    appPassword: "h19bWpWSO8XsJZu89Cf8ntp"
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
